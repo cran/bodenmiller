64b3be61f9033cba70cf13eea88f1a7e931b0938
Yann Abraham <yann.abraham@gmail.com>
2021-04-11 14:20:06 +0000

    version 0.1.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Profiling of Peripheral Blood Monocytes using CyTOF
 
-CyTOF enables the profiling of up to 100 parameters at the single cell level. Bodenmiller *et al* published the profiling of **P**eripheral **B**lood **M**ononuclear **C**ells (PBMCs) using 9 phenotypic and 14 functional markers. PBMCS are stimulated with 12 treatments and treated with 8 doses of various inhibitors, then profiled using CyTOF. PBMCs are manually gated using the phenotypic markers, identifying 14 populations for which the activity of different signaling networks can be assessed using the functional markers. More details are available in [Bodenmiller *et al* Nat Biotech 2012](http://www.nature.com/nbt/journal/v30/n9/full/nbt.2317.html).
+CyTOF enables the profiling of up to 100 parameters at the single cell level. Bodenmiller *et al* published the profiling of **P**eripheral **B**lood **M**ononuclear **C**ells (PBMCs) using 9 phenotypic and 14 functional markers. PBMCS are stimulated with 12 treatments and treated with 8 doses of various inhibitors, then profiled using CyTOF. PBMCs are manually gated using the phenotypic markers, identifying 14 populations for which the activity of different signaling networks can be assessed using the functional markers. More details are available in [Bodenmiller *et al* Nat Biotech 2012](https://www.nature.com/articles/nbt.2317/).
 
 To assess the efficiency of high-dimensional data analysis and visualization algorithms, the samples corresponding to untreated PBMCs, either unstimulated or after stimulation with BCR/FcR-XL, PMA/Ionomycin
 or vanadate are made available as 2 datasets:
@@ -13,7 +13,7 @@ For each dataset there are 3 additional matrix:
 
  * `refPhenotMat` and `untreatedPhenoMat` the signal intensity for the 9 phenotypic channels
  * `refFuncMat` and `untreatedFuncMat` the signal intensity for the 14 functional channels
- * `refAnnots` and `untreatedAnnots` the source file, cell type assignement and stimulation (where appropriate)
+ * `refAnnots` and `untreatedAnnots` the source file, cell type assignment and stimulation (where appropriate)
 
 Install it from github with:
 
--- inst/doc/bodenmiller.R
+++ inst/doc/bodenmiller.R
@@ -1,7 +1,8 @@
-## ----setup,echo=FALSE,include=FALSE--------------------------------------
+## ----setup,echo=FALSE,include=FALSE-------------------------------------------
 library(knitr)
 library(bodenmiller)
 library(ggplot2)
+library(cytofan)
 library(dplyr)
 library(reshape2)
 library(RColorBrewer)
@@ -10,16 +11,7 @@ knitr::opts_chunk$set(warning=FALSE,
                       fig.keep='high',
                       fig.align='center')
 
-do.fan <- function(x,step=0.01) {
-  data.frame(ymin=quantile(x,probs=seq(0,1,step))[-length(seq(0,1,step))],
-             ymax=quantile(x,probs=seq(0,1,step))[-1],
-             id=seq(1,length(seq(step,1,step))),
-             percent=abs(seq(step,1,step)-0.5))
-}
-
-scale_fill_fan <- function(...) scale_fill_gradientn(colours=rev(brewer.pal(9,'Oranges')) )
-
-## ----ref_pheno_boxplot---------------------------------------------------
+## ----ref_pheno_boxplot--------------------------------------------------------
 data(refPhenoMat)
 refPhenoFrame <- melt(refPhenoMat)
 names(refPhenoFrame) <- c('cell_id','channel','value')
@@ -27,7 +19,7 @@ ggplot(data=refPhenoFrame,aes(x=channel,y=value))+
   geom_boxplot()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-## ----annots--------------------------------------------------------------
+## ----annots-------------------------------------------------------------------
 data('refAnnots')
 refPhenoFrame$Cells <- rep(refAnnots$Cells,ncol(refPhenoMat))
 cell.colors <- setNames(c('#9CA5D5','#0015C5','#5B6CB4','#BFC5E8','#C79ED0','#850094',
@@ -37,7 +29,7 @@ cell.colors <- setNames(c('#9CA5D5','#0015C5','#5B6CB4','#BFC5E8','#C79ED0','#85
                           'cd14+hladr-','cd14+hladrhigh','cd14+hladrmid','cd14+surf-',
                           'cd4+','cd8+','dendritic','igm-','igm+','nk'))
 
-## ----ref_pheno_pop_boxplot,fig.width=6,fig.height=4----------------------
+## ----ref_pheno_pop_boxplot,fig.width=6,fig.height=4---------------------------
 cd7.pops <- refPhenoFrame %>% filter(channel=='CD7')
 ggplot(data=cd7.pops,
        aes(x=Cells,y=value,fill=Cells))+
@@ -46,29 +38,27 @@ ggplot(data=cd7.pops,
   guides(fill=FALSE)+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-## ----ref_pheno_sub_fan---------------------------------------------------
-ggplot(refPhenoFrame %>% filter(Cells=='cd4+') %>% group_by(Cells,channel) %>% do(do.fan(.$value)),
-                   aes(x=channel,fill=percent,group=id))+
-  geom_ribbon(aes(ymin=ymin,ymax=ymax))+
-  guides(fill=F)+
+## ----ref_pheno_sub_fan--------------------------------------------------------
+ggplot(refPhenoFrame %>% 
+         filter(Cells=='cd4+'),
+       aes(x=channel,y=value))+
+  geom_fan()+
   facet_wrap(~Cells)+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  scale_fill_fan()
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-## ----ref_func_sub_fan,fig.width=5,fig.height=3---------------------------
+## ----ref_func_sub_fan,fig.width=5,fig.height=3--------------------------------
 data(refFuncMat)
 refFuncFrame <- melt(refFuncMat)
 names(refFuncFrame) <- c('cell_id','channel','value')
 refFuncFrame$Cells <- rep(refAnnots$Cells,ncol(refFuncMat))
-ggplot(refFuncFrame %>% filter(Cells=='cd4+') %>% group_by(Cells,channel) %>% do(do.fan(.$value)),
-                   aes(x=channel,fill=percent,group=id))+
-  geom_ribbon(aes(ymin=ymin,ymax=ymax))+
+ggplot(refFuncFrame %>% 
+         filter(Cells=='cd4+'),
+       aes(x=channel,y=value))+
+  geom_fan()+
   facet_wrap(~Cells)+
-  guides(fill=FALSE)+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  scale_fill_fan()
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-## ----untreated_func------------------------------------------------------
+## ----untreated_func-----------------------------------------------------------
 data('untreatedFuncMat')
 data('untreatedAnnots')
 untreatedFuncFrame <- melt(untreatedFuncMat)
@@ -76,19 +66,19 @@ names(untreatedFuncFrame) <- c('cell_id','channel','value')
 untreatedFuncFrame$Cells <- rep(untreatedAnnots$Cells,ncol(untreatedFuncMat))
 untreatedFuncFrame$Treatment <- rep(untreatedAnnots$Treatment,ncol(untreatedFuncMat))
 
-## ----un_func_sub_fan,fig.width=6,fig.height=6----------------------------
+## ----un_func_sub_fan,fig.width=6,fig.height=6---------------------------------
 refFuncLine <- refFuncFrame %>% filter(Cells=='cd4+') %>% group_by(Cells,channel) %>% summarise(value=median(value))
 refFuncLine <- do.call(rbind,lapply(seq(1,3),function(x) refFuncLine))
 refFuncLine$Treatment <- rep(levels(untreatedFuncFrame$Treatment),each=nlevels(refFuncLine$channel))
 refFuncLine$percent <- 0
 refFuncLine$id <- 'ref'
-ggplot(untreatedFuncFrame %>% filter(Cells=='cd4+') %>% group_by(Treatment,Cells,channel) %>% do(do.fan(.$value)),
-                   aes(x=channel,fill=percent,group=id))+
-  geom_ribbon(aes(ymin=ymin,ymax=ymax))+
-  geom_line(data=refFuncLine,aes(y=value),
+ggplot(untreatedFuncFrame %>% 
+         filter(Cells=='cd4+'),
+       aes(x=channel,y=value))+
+  geom_fan()+
+  geom_line(data=refFuncLine,
+            aes(y=value,group=id),
             col='black',linetype=4)+
-  guides(fill=F)+
   facet_wrap(~Cells*Treatment,ncol=2,scale='free_x')+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  scale_fill_fan()
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
